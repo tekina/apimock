6eacf28646daf0a2665ad0d20caaefad3521065f
Aniket Garg <aniketgarg@outlook.com>
2017-08-08 13:37:42 +0530

    change text in create dashboard

--- app/admin/api.rb
+++ app/admin/api.rb
@@ -24,9 +24,9 @@ ActiveAdmin.register Api do
   form do |f|
     f.inputs do
       f.input :endpoint, as: :string, label: "Endpoint (eg: /v4/home/cities) - leading '/' required"
-      f.input :request_type, as: :select, label: "Request Type (get, post, put, delete etc.)", collection: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], include_blank: false
-      f.input :required_params, as: :string, label: "Required params (eg: platform, version) - comma separated"
-      f.input :required_headers, as: :string, label: "Required headers (eg: auth-token) - comma separated (Work in Progress, not functional)"
+      f.input :request_type, as: :select, label: "Request Type", collection: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'], include_blank: false
+      f.input :required_params, as: :string, label: "Required params (eg: platform, version) - csv"
+      f.input :required_headers, as: :string, label: "Required headers (eg: auth-token) - csv (Work in Progress, not functional)"
       f.input :success_response, as: :text, label: "Success Response (JSON format only)"
       f.input :error_response, as: :text, label: "Error Response (Work in Progress, not functional)"
       # f.input :submit
