14c88a53743a27ded4e5d8f6764d96624acd3de7
Aniket Garg <aniketgarg@outlook.com>
2017-08-08 13:01:58 +0530

    change endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  match "api/*url" => "application#run", via: [:get, :post, :put, :patch, :delete]
+  match "mock/*url" => "application#run", via: [:get, :post, :put, :patch, :delete]
 end
